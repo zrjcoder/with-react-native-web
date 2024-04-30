c3e3405dd67cb9bfcfcb16b00390e1631317947f
张儒杰 <gua@outlook.sg>
2024-04-30 12:55:15 +0800

    feat: init

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { View } from 'react-native-web'
+import Image from 'next/Image'
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flexGrow: 1,
-    justifyContent: "center",
-  },
-  link: {
-    color: "blue",
-  },
-  textContainer: {
-    alignItems: "center",
-    marginTop: 16,
-  },
-  text: {
-    alignItems: "center",
-    fontSize: 24,
-    marginBottom: 24,
-  },
-});
-
-export default function App(props) {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text accessibilityRole="header" style={styles.text}>
-        React Native for Web & Next.js
-      </Text>
-
-      <Text style={styles.link} accessibilityRole="link" href={`/alternate`}>
-        A universal link
-      </Text>
-
-      <View style={styles.textContainer}>
-        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
-          Subheader
-        </Text>
+    <View>
+      <View style={{ height: 180, width: '100%', position: 'relative' }}>
+        <Image src="/images/bg-top.png" fill alt={'十堰美景'} />
       </View>
+
+      <View></View>
     </View>
-  );
+  )
 }
+
+export default App
